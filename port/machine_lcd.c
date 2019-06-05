53061991ae87b113235afed2a9a4309915c48913
SummerGift <SummerGift@qq.com>
2019-06-05 12:04:14 +0800

    【完善】添加 LCD 参数检查

--- port/machine_lcd.c
+++ port/machine_lcd.c
@@ -47,10 +47,12 @@ STATIC void error_check(bool status, const char *msg) {
 
 /// \classmethod \constructor(skin_position)
 ///
-/// Construct an LCD object in the given skin position.  `skin_position` can be 'X' or 'Y', and
-/// should match the position where the LCD pyskin is plugged in.
+/// Construct an LCD object.  
 STATIC mp_obj_t machine_lcd_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
 
+    // check arguments
+    mp_arg_check_num(n_args, n_kw, 0, 0, false);
+    
     // create lcd object
     machine_lcd_obj_t *lcd = m_new_obj(machine_lcd_obj_t);
     lcd->base.type = &machine_lcd_type;
