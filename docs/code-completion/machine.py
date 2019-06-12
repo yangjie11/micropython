e35c553ac2c746134c09b57dc17a56e471cf80f4
SummerGift <SummerGift@qq.com>
2019-06-12 15:15:38 +0800

    [add] LCD module

--- docs/code-completion/machine.py
+++ docs/code-completion/machine.py
@@ -751,6 +751,60 @@ class PWM(object):
         """
         ...
 
+class LCD(object):
+    """
+    在给定总线上构造一个 LCD 对象，无入参，初始化的对象取决于特定硬件。
+    """
+
+    def __init__(self) -> None:
+        """
+        """
+
+    def light(value) -> None:
+        """
+        控制是否开启 LCD 背光，入参为 True 则打开 LCD 背光，入参为 False 则关闭 LCD 背光。
+        """
+        ...
+
+    def fill(color) -> None:
+        """
+        根据给定的颜色填充整个屏幕，支持多种颜色，可以传入的参数有：
+
+        - WHITE BLACK BLUE BRED GRED GBLUE RED MAGENTA GREEN CYAN YELLOW BROWN BRRED GRAY GRAY175 GRAY151 GRAY240
+        """
+        ...
+
+    def pixel(x, y, color) -> None:
+        """
+        向指定的位置（x, y）画点，点的颜色为 color 指定的颜色，可指定的颜色和上一个功能相同。
+        注意：(x, y)  坐标的范围是 0 - 239，使用下面的方法对坐标进行操作时同样需要遵循此限制。
+        """
+        ...
+
+    def text(str, x, y, size) -> None:
+        """
+        在指定的位置（x,y）写入字符串，字符串由 str 指定，字体的大小由 size 指定，size 的大小可为 16，24，32。
+        """
+        ...
+
+    def line(x1, y1, x2, y2) -> None:
+        """
+        在 LCD 上画一条直线，起始地址为 （x1, y1），终点为（x2, y2）。
+        """
+        ...
+
+    def rectangle(x1, y1, x2, y2) -> None:
+        """
+        在 LCD 上画一个矩形，左上角的位置为（x1, y1），右下角的位置为（x2, y2）。
+        """
+        ...
+
+    def circle(x1, y1, r) -> None:
+        """
+        在 LCD 上画一个圆形，圆心的位置为（x1, y1），半径长度为 r。。
+        """
+        ...
+
 def reset() -> None:
     """Resets the device in a manner similar to pushing the external RESET button."""
     ...
