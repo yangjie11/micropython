7b8b7fbb15db58d9b2e4258b3a8ce1f56b467e5e
SummerGift <SummerGift@qq.com>
2019-06-13 16:31:30 +0800

    【添加】基本示例

---
+++ examples/basic/ucollections.py
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2006-2019, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: MIT License
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2019-06-13     SummerGift   first version
+ */
+
+from ucollections import namedtuple
+
+MyTuple = namedtuple("MyTuple", ("id", "name"))
+t1 = MyTuple(1, "foo")
+t2 = MyTuple(2, "bar")
+print(t1.name)
+assert t2.name == t2[1]
+ucollections.OrderedDict(...)
+
+from ucollections import OrderedDict
+
+# To make benefit of ordered keys, OrderedDict should be initialized
+# from sequence of (key, value) pairs.
+d = OrderedDict([("z", 1), ("a", 2)])
+# More items can be added as usual
+d["w"] = 5
+d["b"] = 3
+for k, v in d.items():
+    print(k, v)
