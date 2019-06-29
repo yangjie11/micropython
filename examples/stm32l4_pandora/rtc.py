4463ef37ac6f224e0b56add190b5eb63e2bf156d
chenyong <1521761801@qq.com>
2019-06-29 16:06:38 +0800

    【修改】stm32l4_pandora 示例文件
    Signed-off-by: chenyong <1521761801@qq.com>

--- examples/stm32l4_pandora/rtc.py
+++ examples/stm32l4_pandora/rtc.py
@@ -10,8 +10,8 @@
 
 from machine import RTC
 
-rtc = RTC()                        # Create an RTC device object
-rtc.init((2019,6,5,2,10,22,30,0))  # Set initialization time
-rtc.now()                          # Get the current time
-rtc.deinit()                       # Reset time to January 1, 2015
-rtc.now()                          # Get the current time
+rtc = RTC()                            # Create an RTC device object
+rtc.init((2019,6,5,2,10,22,30,0))      # Set initialization time
+print(rtc.now())                       # Get the current time
+rtc.deinit()                           # Reset time to January 1, 2015
+print(rtc.now())                       # Get the current time
